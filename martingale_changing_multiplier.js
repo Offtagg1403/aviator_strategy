5fa24fab0502ba2f78b4de922444b1a7ae67d54c
bbundi <beunra@yahoo.com>
2023-12-19 14:10:12 +0300

    changing multiplier

---
+++ martingale_changing_multiplier.js
@@ -0,0 +1,127 @@
+
+ const BASE = {
+
+    profit:0,
+    winStreaks:[],
+    loseStreaks: [],
+    gameNumber: 0,
+    prevDay: "",
+    dayTotals :[],
+    MULTI_BOOST:1.2
+
+}
+
+function findBestBet(loss,bet,multiplier){
+    let currentB = bet
+    let currentM = multiplier
+    if(loss>0){//increase multiplier
+        currentM = multiplier*BASE.MULTI_BOOST
+        currentB =  (loss/(currentM-1))  
+    }
+    
+    return [currentB,currentM]
+}
+function maxLossBudget(bet,multiplier,maxLoseStreak){
+    let loss=bet
+    for(x=1;x<maxLoseStreak;x++){
+        [newbet,newmulti] = findBestBet(loss,0,multiplier)  
+        loss += newbet   
+    }
+    return loss
+}
+
+ function newDay(bet,multiplier){
+    const maxLoseStreak = BASE.loseStreaks.map(a => a.length).reduce((a, b) => Math.max(a, b), -Infinity);
+    const loss = maxLossBudget(bet,multiplier,maxLoseStreak)
+    
+    let dayStat = `day ${BASE.prevDay} total games: ${BASE.gameNumber} multiplier ${multiplier} loseStreak ${BASE.loseStreaks.length} maxLoseStreak ${maxLoseStreak} maxloss ${loss} lose count ${BASE.loseStreaks.flat().length}  winStreak: ${BASE.winStreaks.length} win count: ${BASE.winStreaks.flat().length} BASE.profit: ${BASE.profit}`
+    console.log(dayStat)
+
+    BASE.loseStreaks.forEach(l =>{
+        const loss = maxLossBudget(bet,multiplier,l.length)
+  //      console.log(`streak length: ${l.length} loss: ${loss}`)
+    })
+    
+   
+
+  //  console.log("BASE.loseStreaks")
+   // BASE.loseStreaks.forEach(l =>console.log(l.length))
+    
+    BASE.dayTotals.push(dayStat)
+
+    BASE.loseStreaks = []
+    BASE.winStreaks = []
+    currentLoseStreak = []
+    currentWinStreak = []
+    BASE.profit = 0
+    
+ }
+
+  function findStreak(data,bet,multiplier,streakLength){
+ let currentWinStreak = []
+ let currentLoseStreak = []
+ let prevLoss = 0
+ 
+ BASE.prevDay = data[0][0]
+ newmulti = multiplier
+ for (const element of data) {
+    if(BASE.prevDay != element[0]){
+        newDay(bet,newmulti)
+        BASE.prevDay = element[0]
+
+      //  break
+    }
+     [currentBet,newmulti] = findBestBet(prevLoss,bet,multiplier)  
+
+    
+   // if(BASE.gameNumber >= offset){
+
+        if(element[1]>=newmulti){
+
+           if(currentLoseStreak.length>0) BASE.loseStreaks.push(currentLoseStreak)
+            currentLoseStreak = []
+            
+            currentWinStreak.push(BASE.gameNumber)
+
+            if(prevLoss>0){//if it was a recovery dont compund cash out
+                BASE.profit +=prevLoss
+            //    console.log(`%c prevLoss recovered ${prevLoss} BASE.gameNumber: ${BASE.gameNumber} BASE.profit: ${BASE.profit}`,'color: green')
+                prevLoss = 0 
+                currentWinStreak =[]
+                
+            }else{
+                let potentialprofit =currentBet*(Math.pow(newmulti,currentWinStreak.length)-1)               
+              //  console.log(`%c potentialwin BASE.gameNumber: ${BASE.gameNumber} potentialprofit: ${potentialprofit}`,'color: blue')
+            }
+
+            
+
+            if(currentWinStreak.length==streakLength){
+                BASE.profit +=bet*(Math.pow(newmulti,currentWinStreak.length)-1)     
+         //       console.log(`%cday ${element[0]} win BASE.gameNumber: ${BASE.gameNumber} BASE.profit: ${BASE.profit}`,'color: green')
+                BASE.winStreaks.push(currentWinStreak)
+                currentWinStreak =[]
+                
+            }
+        }else{
+
+            prevLoss += currentBet
+
+            BASE.profit-= currentBet
+         //   console.log(`%closs BASE.gameNumber: ${BASE.gameNumber} round lose: ${currentBet} BASE.profit: ${BASE.profit}`,'color: red')
+            currentWinStreak =[]
+            currentLoseStreak.push(BASE.gameNumber)
+
+        }
+
+    //    }
+        
+        BASE.gameNumber++
+
+ }
+ console.log("------------------")
+BASE.dayTotals.forEach(a => console.log(a))
+
+ }
+
+ module.exports = findStreak;
